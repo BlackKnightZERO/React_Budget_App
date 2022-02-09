2bea7d33f732a6067ba918a29bdf4cd1c72008d5
Arif Faysal <ariffaysal101@gmail.com>
2022-02-08 20:08:03 -0500

    useLocalStorage-hook-added

--- src/context/BudgetsContext.js
+++ src/context/BudgetsContext.js
@@ -1,30 +1,75 @@
 import React, { useContext } from 'react';
+import { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
 
 const BudgetsContext = React.createContext();
 
-export function useBudgets() {
+export const useBudgets = () => {
     return useContext(BudgetsContext)
 }
 
+// {
+//     id:,
+//     name:,
+//     max:
+// }
+// {
+//     id:,
+//     budgetId:,
+//     amount:,
+//     description:
+// }
+
 export const BudgetsProvider = ({ children }) => {
 
-    const budgets = []
-    const expenses = []
+    const [budgets, setBudgets] = useState([])
+    const [expenses, setExpenses] = useState([])
 
-    const getBudgetExpenses = () => {
+    const getBudgetExpenses = ( budgetId ) => {
+        return expenses.filter(expense => expense.budgetId === budgetId)
+    }
+
+    const addExpense = ({ budgetId, amount, description }) => {
+
+        let newExpense = {
+            id: uuidV4(),
+            budgetId,
+            amount,
+            description
+        }
+
+        setExpenses(prevExpenses => {
+            return [...prevExpenses, newExpense]
+        })
 
     }
-    const addExpense = () => {
+
+    const addBudget = ( name, max ) => {
+
+        let newBudget = {
+            id: uuidV4(),
+            name,
+            max
+        }
+
+        setBudgets(prevBudgets => {
+            if(prevBudgets.find(budget => budget.name === name)) return prevBudgets
+            return [...prevBudgets, newBudget]
+        })
 
     }
-    const addBudget = () => {
 
+    const deleteBudget = ({ id }) => {
+        // implement uncategorized expenses
+        setBudgets(prevBudgets => {
+            return prevBudgets.filter(budget => budget.id !== id)
+        })
     }
-    const deleteBudget = () => {
 
-    }
-    const deleteExpense = () => {
-        
+    const deleteExpense = ({ id }) => {
+        setExpenses(prevExpenses => {
+            return prevExpenses.filter(expense => expense.id !== id)
+        })
     }
 
     return <BudgetsContext.Provider value={{
